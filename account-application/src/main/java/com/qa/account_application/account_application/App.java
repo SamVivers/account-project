3af6e5a1ba13710e2aaef870fa2d75849d5137f0
SamVivers <viverssamuel@gmail.com>
2019-04-15 14:22:36 +0100

    adding project

---
+++ account-application/src/main/java/com/qa/account_application/account_application/App.java
@@ -0,0 +1,9 @@
+package com.qa.account_application.account_application;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
