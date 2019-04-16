e016ee45d4df115a977246a541f153e409100507
SamVivers <viverssamuel@gmail.com>
2019-04-16 10:35:13 +0100

    Print as JSON using GSON Lib

--- account-application/src/main/java/com/qa/account_application/account_application/App.java
+++ account-application/src/main/java/com/qa/account_application/account_application/App.java
@@ -3,23 +3,31 @@ package com.qa.account_application.account_application;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
+
 public class App 
 {
     public static void main(String[] args) {
         System.out.println( "Hello World!" );
 
-        Account a = new Account("Sam", "Vivers", 111);
-        Account b = new Account("Bob", "Rivers", 142);
-        Account c = new Account("Fred", "Divers", 843);
-        Account d = new Account("Sue", "Livers", 964);
+        Account a111 = new Account("Sam", "Vivers", 111);
+        Account a142 = new Account("Bob", "Rivers", 142);
+        Account a843 = new Account("Fred", "Divers", 843);
+        Account a964 = new Account("Sue", "Livers", 964);
         
         Service lloyds = new Service();
         
-        lloyds.add(a);
-        lloyds.add(b);
-        lloyds.add(c);
-        lloyds.add(d);
+        lloyds.add(a111);
+        lloyds.add(a142);
+        lloyds.add(a843);
+        lloyds.add(a964);
         
-        lloyds.retrive(c);
+        lloyds.retrive(a142);
+        
+        System.out.println(lloyds.m1);
+        
+        Gson gson = new Gson();
+        String json = gson.toJson(lloyds.m1);
+        System.out.println(json);
     }
 }
