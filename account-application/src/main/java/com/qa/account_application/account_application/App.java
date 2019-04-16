272f8e0d210b29d585a15b08b34c0fec80b13228
SamVivers <viverssamuel@gmail.com>
2019-04-16 15:10:23 +0100

    Passes all tests

--- account-application/src/main/java/com/qa/account_application/account_application/App.java
+++ account-application/src/main/java/com/qa/account_application/account_application/App.java
@@ -8,10 +8,24 @@ import com.google.gson.Gson;
 public class App {
     
     public static void main(String[] args) {
-    	setup();
-        
-//        lloyds.retrive(a142);
-//        
+		Account a111 = new Account("Sam", "Vivers", 111);
+	    Account a142 = new Account("Bob", "Rivers", 142);
+	    Account a843 = new Account("Fred", "Divers", 843);
+	    Account a964 = new Account("Sue", "Livers", 964);
+	    Account a445 = new Account("Bob", "Jenkins", 445);
+	      
+	    Service.add(a111);
+	    Service.add(a142);
+	    Service.add(a843);
+	    Service.add(a964);
+	    Service.add(a445);
+	    
+    	Service.retrive(a142);
+    	
+    	System.out.println(Service.fetch(142).contains("Bob "));
+    	
+    	System.out.println(Service.cycle("Bob"));
+       
 //        System.out.println(lloyds.m1);
 //        
 //        Gson gson = new Gson();
@@ -22,18 +36,5 @@ public class App {
 //        m2 = gson.fromJson(json, HashMap.class);
 //        System.out.println(m2);
     }
-    
-    public static void setup() {
-        Account a111 = new Account("Sam", "Vivers", 111);
-        Account a142 = new Account("Bob", "Rivers", 142);
-        Account a843 = new Account("Fred", "Divers", 843);
-        Account a964 = new Account("Sue", "Livers", 964);
-        Account a445 = new Account("Bob", "Jenkins", 445);
-        
-        Service.add(a111);
-        Service.add(a142);
-        Service.add(a843);
-        Service.add(a964);
-        Service.add(a445);
-    }
+
 }
